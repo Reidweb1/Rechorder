11b4d11fc80da1b83d8ff7aaf97d638363d69a24
Reid Weber <reid_weber@hotmail.com>
2014-12-10 11:46:09 -0800

    Fixed ChordTableSeeder

--- ChordBook/ChordTableSeeder.m
+++ ChordBook/ChordTableSeeder.m
@@ -42,9 +42,9 @@
 }
 
 - (NSMutableArray *) fillChordImages {
-    NSArray *keys = @[@"A", @"Am", @"B", @"Bm", @"C", @"Cm", @"D", @"Dm", @"E", @"Em", @"F", @"Fm",@"F#", @"F#m", @"G", @"Gm"];
+    NSArray *keys = @[@"A", @"A7", @"Amaj7", @"Am", @"Am7", @"B", @"B7", @"Bm", @"Bb", @"C", @"C7", @"Cmaj7", @"Cm", @"D", @"D7", @"Dmaj7", @"Dm", @"Dm7", @"Db", @"Dbm", @"E", @"E7", @"Em", @"Em7", @"F", @"Fmaj7", @"Fm", @"F#", @"F#m", @"G", @"G7", @"Gm", @"G#m"];
     
-    NSArray *pics = @[[UIImage imageNamed:@"A"], [UIImage imageNamed:@"Am"], [UIImage imageNamed:@"B"], [UIImage imageNamed:@"Bm"], [UIImage imageNamed:@"C"], [UIImage imageNamed:@"Cm"], [UIImage imageNamed:@"D"], [UIImage imageNamed:@"Dm"], [UIImage imageNamed:@"E"], [UIImage imageNamed:@"Em"], [UIImage imageNamed:@"F"], [UIImage imageNamed:@"Fm"], [UIImage imageNamed:@"F#"], [UIImage imageNamed:@"F#m"], [UIImage imageNamed:@"G"], [UIImage imageNamed:@"Gm"]];
+    NSArray *pics = @[[UIImage imageNamed:@"A"], [UIImage imageNamed:@"A7"], [UIImage imageNamed:@"Amaj7"], [UIImage imageNamed:@"Am"], [UIImage imageNamed:@"Am7"], [UIImage imageNamed:@"B"], [UIImage imageNamed:@"B7"], [UIImage imageNamed:@"Bm"], [UIImage imageNamed:@"Bb"], [UIImage imageNamed:@"C"], [UIImage imageNamed:@"C7"], [UIImage imageNamed:@"Cmaj7"], [UIImage imageNamed:@"Cm"], [UIImage imageNamed:@"D"], [UIImage imageNamed:@"D7"], [UIImage imageNamed:@"Dmaj7"], [UIImage imageNamed:@"Dm"], [UIImage imageNamed:@"Dm7"], [UIImage imageNamed:@"Db"], [UIImage imageNamed:@"Dbm"], [UIImage imageNamed:@"E"], [UIImage imageNamed:@"E7"], [UIImage imageNamed:@"Em"], [UIImage imageNamed:@"Em7"], [UIImage imageNamed:@"F"], [UIImage imageNamed:@"Fmaj7"], [UIImage imageNamed:@"Fm"], [UIImage imageNamed:@"F#"], [UIImage imageNamed:@"F#m"], [UIImage imageNamed:@"G"], [UIImage imageNamed:@"G7"], [UIImage imageNamed:@"Gm"], [UIImage imageNamed:@"G#m"]];
     
     
     NSMutableArray *chordObjects = [[NSMutableArray alloc] init];
