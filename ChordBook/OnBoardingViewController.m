e13480637c8f48f378df4c674fb5615fa8b372ab
Reid Weber <reid_weber@hotmail.com>
2015-01-12 11:03:25 -0800

    Added OnBoarding ViewController

---
+++ ChordBook/OnBoardingViewController.m
@@ -0,0 +1,37 @@
+//
+//  OnBoardingViewController.m
+//  ChordBook
+//
+//  Created by Reid Weber on 1/12/15.
+//  Copyright (c) 2015 Reid Weber. All rights reserved.
+//
+
+#import "OnBoardingViewController.h"
+
+@interface OnBoardingViewController ()
+
+@end
+
+@implementation OnBoardingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
